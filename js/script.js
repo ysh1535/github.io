f5f76abc1d55f75d47afd7ade95dbbdc40d43141
mryou <60817024+ysh1535@users.noreply.github.com>
2023-02-04 23:30:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,36 @@ menu.addEventListener('click', () => {
 window.onscroll = () => {
     menu.classList.add('fa-times');
     navbar.classList.add('nav-toggle');
-}
+};
+
+
+var swiper = new Swiper(".gallery-slider", {
+    loop: true,
+    spaceBetween: 10,
+    grabCursor: true,autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+
 
 
