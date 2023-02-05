26b110e845f9ea2d564fac557be8c826257a21b4
mryou <60817024+ysh1535@users.noreply.github.com>
2023-02-05 21:44:10 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('nav-toggle');
+// menu.addEventListener('click', () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('nav-toggle');
+// });
+
+// window.onscroll = () => {
+//     menu.classList.add('fa-times');
+//     navbar.classList.add('nav-toggle');
+// };
+
+$(document).ready(function(){
+
+    let menu = document.querySelector('#menu-bar');
+    let navbar = document.querySelector('.navbar');
+    
+    menu.addEventListener('click', () =>{
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('nav-toggle');
+    });
+    
+    window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('nav-toggle');
+    }
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.nav').toggleClass('nav-toggle');
+    });
+
+    $('nav ul li a').click(function(){
+        $('.fa-bars').addClass('fa-times');
+        $('.nav').addClass('nav-toggle');
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() < 20){
+            $('header').css('box-shadow','none');
+        }else{
+            $('header').css('box-shadow','0 .2rem .5rem #333');
+        }
+    });
+
 });
 
-window.onscroll = () => {
-    menu.classList.add('fa-times');
-    navbar.classList.add('nav-toggle');
-};
 
 
 var swiper = new Swiper(".gallery-slider", {
@@ -39,6 +75,26 @@ var swiper = new Swiper(".gallery-slider", {
     },
 });
 
-
+var swiper = new Swiper(".image-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    // },
+});
 
 
